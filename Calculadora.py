8104eb9b71788cc179f6b56ab285bbf6cf56f24a
DiogoCarapito <diogo.carapito@icloud.com>
2024-07-23 18:07:58 +0100

    major improvements

--- Calculadora.py
+++ Calculadora.py
@@ -1,188 +1,270 @@
 import streamlit as st
 
+from utils.utils import session_state_initialization, calculos
+
+
 def main():
     st.title("Calculadora Horário MGF")
 
-    st.write("Cálculo da distribuição do tempo de trabalho dos médicos de família dependendo das características da lista.")
-    
-    if st.session_state.get("n_crianças_1_ano") is None:
-        st.session_state["n_crianças_1_ano"] = 20
-    if st.session_state.get("tempo_consulta_crianças_1_ano") is None:
-        st.session_state["tempo_consulta_crianças_1_ano"] = 30
-    if st.session_state.get("n_consultas_crianças_1_ano") is None:
-        st.session_state["n_consultas_crianças_1_ano"] = 6
-    
-    
+    st.write(
+        "Cálculo da distribuição do tempo de trabalho dos médicos de família dependendo das características da lista."
+    )
+
+    session_state_initialization()
+
     st.subheader("Crianças < 1 ano")
-    col_crianças_1_ano_1, col_crianças_1_ano_2, n_crianças_1_ano_3 = st.columns(3, vertical_alignment="bottom")
+    col_crianças_1_ano_1, col_crianças_1_ano_2, n_crianças_1_ano_3 = st.columns(
+        3, vertical_alignment="bottom"
+    )
 
     with col_crianças_1_ano_1:
         st.session_state["n_crianças_1_ano"] = st.number_input(
-            "Número de crianças < 1 ano",
-            step=1,
-            value=20
-            )
+            "Nº de crianças < 1 ano", step=1, value=15
+        )
     with col_crianças_1_ano_2:
         st.session_state["tempo_consulta_crianças_1_ano"] = st.number_input(
-            "Tempo por consulta crinaças < 1 ano",
-            step=5,
-            value=30
-            )
-        
+            "Tempo por consulta crianças < 1 ano", step=5, value=30
+        )
+
     with n_crianças_1_ano_3:
         st.session_state["n_consultas_crianças_1_ano"] = st.number_input(
-            "Número de consultas a crianças com menos de 1 ano por ano",
-            step=1,
-            value=6
-            )
+            "Nº de consultas a crianças com menos de 1 ano por ano", step=1, value=6
+        )
 
     st.subheader("Crianças entre 1 e 2 anos")
 
-    col_crianças_2_ano_1, col_crianças_2_ano_2, n_crianças_2_ano_3 = st.columns(3, vertical_alignment="bottom")
+    col_crianças_2_ano_1, col_crianças_2_ano_2, n_crianças_2_ano_3 = st.columns(
+        3, vertical_alignment="bottom"
+    )
 
     with col_crianças_2_ano_1:
-        st.session_state["n_crianças_1_ano"] = st.number_input(
-            "Número de crianças entre 1 e 2 anos",
-            step=1,
-            value=20
-            )
+        st.session_state["n_crianças_1_2_anos"] = st.number_input(
+            "Nº de crianças entre 1 e 2 anos", step=1, value=17
+        )
     with col_crianças_2_ano_2:
-        st.session_state["tempo_consulta_crianças_1_ano"] = st.number_input(
-            "Tempo por consulta crinaças entre 1 e 2 anos",
-            step=5,
-            value=30
-            )
-        
-    with n_crianças_2_ano_3:
-        st.session_state["n_consultas_crianças_1_ano"] = st.number_input(
-            "Número de consultas a crianças entre 1 e 2 anos por ano",
-            step=1,
-            value=6
-            )
+        st.session_state["tempo_consulta_crianças_1_2_anos"] = st.number_input(
+            "Tempo por consulta crianças entre 1 e 2 anos", step=5, value=30
+        )
 
-    st.subheader("Crianças e jovens (2-18 anos)")
-    
-    col_crianças_jovens_1, col_crianças_jovens_2, col_crianças_jovens_3 = st.columns(3, vertical_alignment="bottom")
+    with n_crianças_2_ano_3:
+        st.session_state["n_consultas_crianças_1_2_anos"] = st.number_input(
+            "Nº de consultas a crianças entre 1 e 2 anos por ano", step=1, value=5
+        )
+
+    st.subheader("Crianças e jovens 2-18 anos")
+
+    col_crianças_jovens_1, col_crianças_jovens_2, col_crianças_jovens_3 = st.columns(
+        3, vertical_alignment="bottom"
+    )
 
     with col_crianças_jovens_1:
         st.session_state["n_crianças_2_18_anos"] = st.number_input(
-            "Número de crianças entre 2 e 18 anos",
-            step=1,
-            value=20
-            )
+            "Nº de crianças/jovens entre 2 e 18 anos", step=1, value=40
+        )
     with col_crianças_jovens_2:
-        st.session_state["tempo_crianças_2_18_anos"] = st.number_input(
-            "Tempo por consulta crinaças entre 2 e 18 anos",
-            step=5,
-            value=30
-            )
-        
+        st.session_state["tempo_consulta_crianças_2_18_anos"] = st.number_input(
+            "Tempo por consulta crianças/jovens entre 2 e 18 anos", step=5, value=30
+        )
+
     with col_crianças_jovens_3:
         st.session_state["n_consultas_crianças_2_18_anos"] = st.number_input(
-            "Número de consultas a crianças entre 2 e 18 anos por ano",
+            "Nº de consultas a crianças/jovens entre 2 e 18 anos por ano",
             step=1,
-            value=6
-            )
-    
+            value=5,
+        )
+
     st.divider()
-    
+
     st.subheader("Mulheres em idade fértil")
-    
-    col_mulheres_idade_fertil_1, col_mulheres_idade_fertil_2, col_mulheres_idade_fertil_3 = st.columns(3, vertical_alignment="bottom")
-    
+
+    (
+        col_mulheres_idade_fertil_1,
+        col_mulheres_idade_fertil_2,
+        col_mulheres_idade_fertil_3,
+    ) = st.columns(3, vertical_alignment="bottom")
+
     with col_mulheres_idade_fertil_1:
         st.session_state["n_mulheres_idade_fertil"] = st.number_input(
-            "Número de mulheres em idade fértil (15-54 anos)",
-            step=1
-            )
+            "Nº de mulheres em idade fértil (15-54 anos)", step=1, value=200
+        )
     with col_mulheres_idade_fertil_2:
-        st.session_state["taxa_utilização"] = st.number_input(
-            "Taxa de utilização de consultas",
-            step=1
-            )
-    
-    with col_mulheres_idade_fertil_3:
         st.session_state["tempo_consulta_mulheres_idade_fertil"] = st.number_input(
-            "Tempo por consulta",
-            step=5,
-            value=30
-            )
-    
-    
+            "Tempo por consulta mulheres em idade fértil (min)", step=5, value=20
+        )
+
+    with col_mulheres_idade_fertil_3:
+        st.session_state["taxa_utilização_mulheres_idade_fertil"] = st.number_input(
+            "Taxa de utilização de consultas (%) anual", step=1, value=50
+        )
+
     st.divider()
-    
+
     st.subheader("Grávidas")
-    
-    col_gravidas_1, col_gravidas_2, col_gravidas_3 = st.columns(3, vertical_alignment="bottom")
+
+    col_gravidas_1, col_gravidas_2, col_gravidas_3 = st.columns(
+        3, vertical_alignment="bottom"
+    )
     with col_gravidas_1:
         st.session_state["n_nascimentos_ano_anterior"] = st.number_input(
-            "Número de nascimentos no ano anterior",
-            step=1
-            )
+            "Nº de nascimentos no ano anterior", step=1
+        )
     with col_gravidas_2:
         st.session_state["tempo_consulta_gravidas"] = st.number_input(
-            "Tempo por consulta gravida",
-            step=5,
-            value=30
-            )
+            "Tempo por consulta gravida", step=5, value=30
+        )
     with col_gravidas_3:
         st.session_state["n_consultas_gravidas"] = st.number_input(
-            "Número de consultas por ano",
-            step=1,
-            value=7
-            )
-
+            "Nº de consultas por ano", step=1, value=7
+        )
 
     st.divider()
-    
+
     st.subheader("Diabéticos")
-    
-    col_diabeticos_1, col_diabeticos_2, col_diabeticos_3 = st.columns(3, vertical_alignment="bottom")
-    
+
+    col_diabeticos_1, col_diabeticos_2, col_diabeticos_3 = st.columns(
+        3, vertical_alignment="bottom"
+    )
+
     with col_diabeticos_1:
         st.session_state["n_diabeticos"] = st.number_input(
-            "Número de diabéticos",
-            step=1,
-            value=100
-            )
+            "Nº de diabéticos", step=1, value=100
+        )
     with col_diabeticos_2:
-        st.session_state["tempo_diabeticos"] = st.number_input(
-            "Tempo por consulta diabéticos",
-            step=5,
-            value=30
-            )
+        st.session_state["tempo_consulta_diabeticos"] = st.number_input(
+            "Tempo por consulta diabéticos", step=5, value=30
+        )
     with col_diabeticos_3:
         st.session_state["n_consultas_diabeticos"] = st.number_input(
-            "Número de consultas por ano",
-            step=1,
-            value=2
-            )
-        
+            "Nº de consultas por ano", step=0.25, value=2.0
+        )
 
     st.divider()
 
-    st.session_state["n_horas_base"] = st.slider(
-        "Número de horas base",
-        0,
-        60,
-        40
-        )
-    
-    st.session_state["n_horas_nao_assistencial"] = st.slider(
-        "Número de horas não assistencial",
-        0, 40, 4
-    )
-    
-    st.session_state["p_doneça_aguda"] = st.slider(
-        "Percentagem de doença aguda",
-        0,
-        100,
-        30
-        )
-    
-    
-st.sidebar.title("Horario")
+    st.session_state["n_horas_semanal_base"] = st.slider("Nº de horas base", 0, 60, 40)
 
+    st.session_state["n_horas_nao_assistencial"] = st.slider(
+        "Nº de horas não assistencial", 0, 40, 4
+    )
+
+    col_doença_aguda_1, col_doença_aguda_2 = st.columns(2, vertical_alignment="bottom")
+    with col_doença_aguda_1:
+        st.session_state["p_doneça_aguda"] = st.slider(
+            "Percentagem de doença aguda", 0, 100, 30
+        )
+
+    with col_doença_aguda_2:
+        st.session_state["tempo_consulta_doença_aguda"] = st.number_input(
+            "Tempo por consulta doença aguda", 0, 30, 15
+        )
+
+    st.session_state["n_semanas_trabalho"] = st.slider(
+        "Nº de semanas de trabalho por ano", 0, 52, 44
+    )
+
+    st.divider()
+
+    dados = {
+        "n_crianças_1_ano": st.session_state["n_crianças_1_ano"],
+        "tempo_consulta_crianças_1_ano": st.session_state[
+            "tempo_consulta_crianças_1_ano"
+        ],
+        "n_consultas_crianças_1_ano": st.session_state["n_consultas_crianças_1_ano"],
+        "n_crianças_1_2_anos": st.session_state["n_crianças_1_2_anos"],
+        "tempo_consulta_crianças_1_2_anos": st.session_state[
+            "tempo_consulta_crianças_1_2_anos"
+        ],
+        "n_consultas_crianças_1_2_anos": st.session_state[
+            "n_consultas_crianças_1_2_anos"
+        ],
+        "n_crianças_2_18_anos": st.session_state["n_crianças_2_18_anos"],
+        "tempo_consulta_crianças_2_18_anos": st.session_state[
+            "tempo_consulta_crianças_2_18_anos"
+        ],
+        "n_consultas_crianças_2_18_anos": st.session_state[
+            "n_consultas_crianças_2_18_anos"
+        ],
+        "n_mulheres_idade_fertil": st.session_state["n_mulheres_idade_fertil"],
+        "taxa_utilização_mulheres_idade_fertil": st.session_state[
+            "taxa_utilização_mulheres_idade_fertil"
+        ],
+        "tempo_consulta_mulheres_idade_fertil": st.session_state[
+            "tempo_consulta_mulheres_idade_fertil"
+        ],
+        "n_nascimentos_ano_anterior": st.session_state["n_nascimentos_ano_anterior"],
+        "tempo_consulta_gravidas": st.session_state["tempo_consulta_gravidas"],
+        "n_consultas_gravidas": st.session_state["n_consultas_gravidas"],
+        "n_diabeticos": st.session_state["n_diabeticos"],
+        "tempo_consulta_diabeticos": st.session_state["tempo_consulta_diabeticos"],
+        "n_consultas_diabeticos": st.session_state["n_consultas_diabeticos"],
+        "n_horas_semanal_base": st.session_state["n_horas_semanal_base"],
+        "n_horas_nao_assistencial": st.session_state["n_horas_nao_assistencial"],
+        "p_doneça_aguda": st.session_state["p_doneça_aguda"],
+        "tempo_consulta_doença_aguda": st.session_state["tempo_consulta_doença_aguda"],
+        "n_semanas_trabalho": st.session_state["n_semanas_trabalho"],
+    }
+
+    resultados = calculos(dados)
+
+    st.header("Resultados detalhados")
+
+    st.divider()
+
+    st.metric("Nº de horas de trabalho anual: ", resultados["total_horas_trabalho_ano"])
+
+    st.divider()
+    st.header("Saúde Infantil")
+    st.metric(
+        "Nº total de horas dedicadas à saúde infantil: ",
+        resultados["horas_total_saude_infantil"],
+    )
+    st.metric(
+        "Nº total de consultas de saúde infantil: ",
+        resultados["n_consultas_total_saude_infantil"],
+    )
+
+    st.subheader("Crianças < 1 ano")
+    st.metric(
+        "Nº de horas semanais dedicadas a crianças < 1 ano: ",
+        resultados["horas_semana_crianças_1_ano"],
+    )
+    st.metric(
+        "Nº de conusltas semanais a crianças < 1 anos",
+        resultados["n_consultas_semanal_crianças_1_ano"],
+    )
+
+    st.subheader("Crianças entre 1 e 2 anos")
+    st.metric(
+        "Nº de horas semanais dedicadas a crianças entre 1 e 2 anos: ",
+        resultados["horas_semana_crianças_1_2_anos"],
+    )
+    st.metric(
+        "Nº de consultas semanais a crianças entre 1 e 2 anos",
+        resultados["n_consultas_semanal_crianças_1_2_anos"],
+    )
+
+    st.subheader("Crianças e jovens entre 2 e 18 anos")
+    st.metric(
+        "Nº de horas semanais dedicadas a crianças e jovens entre 2 e 18 anos: ",
+        resultados["horas_semana_crianças_2_18_anos"],
+    )
+    st.metric(
+        "Nº de consultas semanais a crianças e jovens entre 2 e 18 anos",
+        resultados["n_consultas_semanal_crianças_2_18_anos"],
+    )
+
+    # Sidebar
+
+    st.sidebar.title("Resultados")
+
+    st.sidebar.subheader("Saúde Infantil")
+    st.sidebar.metric(
+        "Nº de horas semanais Saúde Infantil: ",
+        resultados["horas_total_saude_infantil"],
+    )
+    st.sidebar.metric(
+        "Nº de consultas semanais Saúde Infantil: ",
+        resultados["n_consultas_total_saude_infantil"],
+    )
 
 
 if __name__ == "__main__":
--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,134 @@
-def func():
-    return None
+import streamlit as st
+
+
+@st.cache_data
+def calculos(dados):
+    total_horas_trabalho_ano = (
+        dados["n_semanas_trabalho"] * dados["n_horas_semanal_base"]
+    )
+
+    horas_semana_crianças_1_ano = round(
+        dados["n_crianças_1_ano"]
+        * dados["n_consultas_crianças_1_ano"]
+        * dados["tempo_consulta_crianças_1_ano"]
+        / 60
+        / dados["n_semanas_trabalho"],
+        2,
+    )
+    n_consultas_semanal_crianças_1_ano = round(
+        dados["n_crianças_1_ano"]
+        * dados["n_consultas_crianças_1_ano"]
+        / dados["n_semanas_trabalho"],
+        1,
+    )
+
+    horas_semana_crianças_1_2_anos = round(
+        dados["n_crianças_1_2_anos"]
+        * dados["n_consultas_crianças_1_2_anos"]
+        * dados["tempo_consulta_crianças_1_2_anos"]
+        / 60
+        / dados["n_semanas_trabalho"],
+        2,
+    )
+    n_consultas_semanal_crianças_1_2_anos = round(
+        dados["n_crianças_1_2_anos"]
+        * dados["n_consultas_crianças_1_2_anos"]
+        / dados["n_semanas_trabalho"],
+        1,
+    )
+
+    horas_semana_crianças_2_18_anos = round(
+        dados["n_crianças_2_18_anos"]
+        * dados["n_consultas_crianças_2_18_anos"]
+        * dados["tempo_consulta_crianças_2_18_anos"]
+        / 60
+        / dados["n_semanas_trabalho"],
+        2,
+    )
+    n_consultas_semanal_crianças_2_18_anos = round(
+        dados["n_crianças_2_18_anos"]
+        * dados["n_consultas_crianças_2_18_anos"]
+        / dados["n_semanas_trabalho"],
+        1,
+    )
+
+    horas_total_saude_infantil = round(
+        horas_semana_crianças_1_ano
+        + horas_semana_crianças_1_2_anos
+        + horas_semana_crianças_2_18_anos,
+        1,
+    )
+    n_consultas_total_saude_infantil = round(
+        n_consultas_semanal_crianças_1_ano
+        + n_consultas_semanal_crianças_1_2_anos
+        + n_consultas_semanal_crianças_2_18_anos,
+        1,
+    )
+
+    return {
+        "total_horas_trabalho_ano": total_horas_trabalho_ano,
+        "horas_semana_crianças_1_ano": horas_semana_crianças_1_ano,
+        "n_consultas_semanal_crianças_1_ano": n_consultas_semanal_crianças_1_ano,
+        "horas_semana_crianças_1_2_anos": horas_semana_crianças_1_2_anos,
+        "n_consultas_semanal_crianças_1_2_anos": n_consultas_semanal_crianças_1_2_anos,
+        "horas_semana_crianças_2_18_anos": horas_semana_crianças_2_18_anos,
+        "n_consultas_semanal_crianças_2_18_anos": n_consultas_semanal_crianças_2_18_anos,
+        "horas_total_saude_infantil": horas_total_saude_infantil,
+        "n_consultas_total_saude_infantil": n_consultas_total_saude_infantil,
+    }
+
+
+@st.cache_data
+def session_state_initialization():
+    if "n_crianças_1_ano" not in st.session_state:
+        st.session_state["n_crianças_1_ano"] = 15
+    if "tempo_consulta_crianças_1_ano" not in st.session_state:
+        st.session_state["tempo_consulta_crianças_1_ano"] = 30
+    if "n_consultas_crianças_1_ano" not in st.session_state:
+        st.session_state["n_consultas_crianças_1_ano"] = 6
+
+    if "n_crianças_1_2_anos" not in st.session_state:
+        st.session_state["n_crianças_1_2_anos"] = 17
+    if "tempo_consulta_crianças_1_2_anos" not in st.session_state:
+        st.session_state["tempo_consulta_crianças_1_2_anos"] = 30
+    if "n_consultas_crianças_1_2_anos" not in st.session_state:
+        st.session_state["n_consultas_crianças_1_2_anos"] = 5
+
+    if "n_crianças_2_18_anos" not in st.session_state:
+        st.session_state["n_crianças_2_18_anos"] = 40
+    if "tempo_consulta_crianças_2_18_anos" not in st.session_state:
+        st.session_state["tempo_consulta_crianças_2_18_anos"] = 30
+    if "n_consultas_crianças_2_18_anos" not in st.session_state:
+        st.session_state["n_consultas_crianças_2_18_anos"] = 5
+
+    if "n_mulheres_idade_fertil" not in st.session_state:
+        st.session_state["n_mulheres_idade_fertil"] = 200
+    if "taxa_utilização_mulheres_idade_fertil" not in st.session_state:
+        st.session_state["taxa_utilização_mulheres_idade_fertil"] = 50
+    if "tempo_consulta_mulheres_idade_fertil" not in st.session_state:
+        st.session_state["tempo_consulta_mulheres_idade_fertil"] = 20
+
+    if "n_nascimentos_ano_anterior" not in st.session_state:
+        st.session_state["n_nascimentos_ano_anterior"] = 5
+    if "tempo_consulta_gravidas" not in st.session_state:
+        st.session_state["tempo_consulta_gravidas"] = 30
+    if "n_consultas_gravidas" not in st.session_state:
+        st.session_state["n_consultas_gravidas"] = 7
+
+    if "n_diabeticos" not in st.session_state:
+        st.session_state["n_diabeticos"] = 100
+    if "tempo_consulta_diabeticos" not in st.session_state:
+        st.session_state["tempo_consulta_diabeticos"] = 30
+    if "n_consultas_diabeticos" not in st.session_state:
+        st.session_state["n_consultas_diabeticos"] = 2
+
+    if "n_horas_base" not in st.session_state:
+        st.session_state["n_horas_base"] = 40
+    if "n_horas_nao_assistencial" not in st.session_state:
+        st.session_state["n_horas_nao_assistencial"] = 4
+    if "p_doneça_aguda" not in st.session_state:
+        st.session_state["p_doneça_aguda"] = 30
+    if "tempo_consulta_doença_aguda" not in st.session_state:
+        st.session_state["tempo_consulta_doença_aguda"] = 15
+    if "n_semanas_trabalho" not in st.session_state:
+        st.session_state["n_semanas_trabalho"] = 44
